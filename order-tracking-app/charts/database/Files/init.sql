6a02c7c3e21f3cbef37a330fcd47303feb1d653b
sina <ksina8279@gmail.com>
2025-01-11 20:24:12 +0330

    Created everything and finished creating master-slave db

--- order-tracking-app/charts/database/Files/init.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS ORDER_TB(
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT,
-    prod_count BIGINT,
-    product_name VARCHAR(255),
-    address VARCHAR(255),
-    status VARCHAR(255)
-);
